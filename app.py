3af3ee0d96fe5f277365648710e378f4bc763bea
Eric Mas Moncusi <err8029@gmail.com>
2016-04-09 02:45:04 +0200

    show users functionality added

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, request
 from flask import render_template
+import sqlite3
 
 app = Flask(__name__)
 
@@ -11,9 +12,34 @@ def index():
 def insert_user():
     return render_template('insert_user.html')
 
+@app.route('/user_register', methods=['POST'])
+def user_register():
+    conn = sqlite3.connect('mydatabase.db')
+    username = request.form.get('username')
+    fullname = request.form.get('fullname')
+    email = request.form.get('email')
+    password = request.form.get('password')
+    cursor=conn.execute("insert into users (username,fullname,email,password) values (?, ?, ?, ?)",
+                       (username,
+                        fullname,
+                        email,
+                        password))
+    conn.commit()
+    conn.close()
+    return render_template('user_inserted.html',
+                           username=username,
+                           fullname=fullname,
+                           email=email,
+                           password=password)
+
 @app.route('/show_users')
 def show_users():
-    return render_template('show_users.html')
+    conn = sqlite3.connect('mydatabase.db')
+    cursor=conn.execute("select username,fullname,email,password from users")
+    data=[row for row in cursor]
+    conn.close()
+    return render_template('show_users.html',
+                           data=data)
 
 @app.route('/login')
 def login_form():
